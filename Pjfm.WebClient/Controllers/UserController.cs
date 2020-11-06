247edd33ef36563a71ebabd077beb1d158cd85cb
Meindert <meindertduin@hotmail.com>
2020-11-06 16:14:17 +0100

    endpoint for user profiles

---
+++ Pjfm.WebClient/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Pjfm.Application.MediatR.Users.Queries;
+
+namespace pjfm.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        [HttpGet("list")]
+        public async Task<IActionResult> GetAllUserProfiles()
+        {
+            var result = await _mediator.Send(new GetAllUserProfileQuery());
+
+            return Ok(result.Data);
+        }   
+    }
+}
