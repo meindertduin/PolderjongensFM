bb47dcb45b10b7dfdab13a61545f80676a7586fa
Meindert <meindertduin@hotmail.com>
2020-12-16 15:24:35 +0100

    tests playbacklistenermanager class

---
+++ Pjfm.WebClient.UnitTests/ServicesTests/PlaybackListenerManagerTest.cs
@@ -0,0 +1,89 @@
+﻿using Moq;
+using Pjfm.Application.Identity;
+using Pjfm.Domain.Interfaces;
+using Pjfm.WebClient.Services;
+using Xunit;
+
+namespace Pjfm.WebClient.UnitTests.ServicesTests
+{
+    public class PlaybackListenerManagerTest
+    {
+        private PlaybackListenerManager _playbackListenerManager;
+
+        public PlaybackListenerManagerTest()
+        {
+            _playbackListenerManager = new PlaybackListenerManager(new Mock<ISpotifyPlaybackManager>().Object,
+                new Mock<IPlaybackController>().Object);
+        }
+        
+        [Fact]
+        public void RemoveListener_AfterAddingValidListener_ReturnsNotNull()
+        {
+                        
+            var fakeUserId = "123";
+            var fakeUser = new ApplicationUser("fake@mail.com"){ Id = fakeUserId};
+
+            _playbackListenerManager.AddListener(fakeUser);
+            var removedUser = _playbackListenerManager.RemoveListener(fakeUserId);
+            Assert.NotNull(removedUser);
+        }
+
+        [Fact]
+        public void TrySetTimedListener_WithValidTimeAndId_ReturnsTrue()
+        {
+            var fakeUserId = "123";
+            var minutes = 5;
+
+            var setTimedListenerResult = _playbackListenerManager.TrySetTimedListener(fakeUserId, minutes);
+            
+            Assert.True(setTimedListenerResult);
+        }
+
+        [Fact]
+        public void TryRemoveTimedListener_AfterAddingTimedListener_ReturnsTrue()
+        {
+            var fakeUserId = "123";
+            var minutes = 5;
+            _playbackListenerManager.TrySetTimedListener(fakeUserId, minutes);
+            var removeResult = _playbackListenerManager.TryRemoveTimedListener(fakeUserId);
+            Assert.True(removeResult);
+        }
+
+        [Fact]
+        public void TryRemoveTimedListener_WithoutAddingUser_ReturnsFalse()
+        {
+            var fakeUserId = "123";
+            var removeResult = _playbackListenerManager.TryRemoveTimedListener(fakeUserId);
+            Assert.False(removeResult);
+        }
+
+        [Fact]
+        public void TryRemoveTimedListener_TryRemoveUserTwice_ReturnsFalseSecondTime()
+        {
+            var fakeUserId = "123";
+            var minutes = 5;
+            _playbackListenerManager.TrySetTimedListener(fakeUserId, minutes);
+            _playbackListenerManager.TryRemoveTimedListener(fakeUserId);
+            var removeResult = _playbackListenerManager.TryRemoveTimedListener(fakeUserId);
+            Assert.False(removeResult);
+        }
+
+        [Fact]
+        public void GetUserSubscribeTime_AfterAddingUserId5Minutes_ReturnsFive()
+        {
+            var fakeUserId = "123";
+            var minutes = 5;
+            _playbackListenerManager.TrySetTimedListener(fakeUserId, minutes);
+            var subscribeTime = _playbackListenerManager.GetUserSubscribeTime(fakeUserId);
+            Assert.Equal(minutes, subscribeTime);
+        }
+
+        [Fact]
+        public void GetUserSubscribeTime_WithNonAddedUserId_ReturnsNull()
+        {
+            var fakeUserId = "123";
+            var subscribeTime = _playbackListenerManager.GetUserSubscribeTime(fakeUserId);
+            Assert.Null(subscribeTime);
+        }
+    }
+}
