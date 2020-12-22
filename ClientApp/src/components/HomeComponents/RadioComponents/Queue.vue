89d077e4352c3a7ad58ba86a3560ef7e7671da4f
Meindert <meindertduin@hotmail.com>
2020-12-22 18:26:05 +0100

    chip that displays the playback mode

--- ClientApp/src/components/HomeComponents/RadioComponents/Queue.vue
+++ ClientApp/src/components/HomeComponents/RadioComponents/Queue.vue
@@ -6,14 +6,18 @@
               outlined 
               round
           >             
-              <span class="overline grey--text">Wachtrij</span><br>
-                  <v-list dense>
+              <v-row>
+                <v-chip class="mt-2 ml-4" :color="modeChipColor">{{playbackStateString}}</v-chip>
+              </v-row>
+              <div>
+                <span class="overline grey--text">Wachtrij</span><br>
+                <v-list dense>
                   <v-list-item-group
-                    class=""
+                      class=""
                   >
                     <v-list-item
-                      v-for="(item, i) in queue.slice(0, 10)"
-                      :key="i"
+                        v-for="(item, i) in queue.slice(0, 10)"
+                        :key="i"
                     >
                       <v-list-item-content>
                         <v-list-item-title>
@@ -23,31 +27,54 @@
                     </v-list-item>
                   </v-list-item-group>
                 </v-list>
+              </div>
           </v-card>
       </v-col>
     </v-row>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
-    import Component from 'vue-class-component'
-    import {Watch} from "vue-property-decorator";
-    import {HubConnectionBuilder, TransferFormat, LogLevel, HubConnection} from "@microsoft/signalr";
-    import {djPlaybackInfo} from "@/common/types";
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import {Watch} from "vue-property-decorator";
+import {playbackState, userPlaybackInfo} from "@/common/types";
 
-    @Component({
+@Component({
         name: 'Queue',
     })
     export default class Queue extends Vue { 
       private queue = [];
-
+      private modeColors = [
+          "purple",
+          "primary",
+          "red",
+      ]
+      
+      private modeChipColor :string = this.modeColors[0]    
+  
       created(){
         this.updateRadio();
       }
 
-      get playbackInfo():djPlaybackInfo{
+      get playbackInfo():userPlaybackInfo{
           return this.$store.getters['playbackModule/getPlaybackInfo'];
       }
+      
+      get playbackStateString():string{
+        const state: playbackState | null =  this.$store.getters['playbackModule/getPlaybackState'];
+        if (state === null) return "Dj-Modues";
+        switch (state){
+          case 0:
+            this.modeChipColor = this.modeColors[0]
+            return "Dj-modus";
+          case 1:
+            this.modeChipColor = this.modeColors[1]
+            return "Verzoekjes-modues"
+          case 2:
+            this.modeChipColor = this.modeColors[2]
+            return "Random-verzoekjes-modus"
+        }
+      } 
       
       @Watch('playbackInfo')
       updateRadio(){
