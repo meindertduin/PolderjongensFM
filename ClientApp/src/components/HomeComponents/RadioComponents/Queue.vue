991a43f9d06890c0f81bc5e9e6f23a48afaedf36
Meindert <meindertduin@hotmail.com>
2020-12-22 19:17:28 +0100

    depending on payback state the queue gets displayed differently

--- ClientApp/src/components/HomeComponents/RadioComponents/Queue.vue
+++ ClientApp/src/components/HomeComponents/RadioComponents/Queue.vue
@@ -6,21 +6,83 @@
               outlined 
               round
           >             
-              <v-row>
+              <v-row class="mb-3">
                 <v-chip class="mt-2 ml-4" :color="modeChipColor">{{playbackStateString}}</v-chip>
               </v-row>
-              <div>
+              <div class="random-queue" v-if="playbackState === 2">
+                <div v-if="queue.filter(x => x.queueNum === 0).length > 0">
+                  <span class="overline grey--text">Prioriteit tracks</span><br>
+                  <v-list dense>
+                    <v-list-item-group>
+                      <v-list-item
+                          v-for="(item, i) in queue.filter(x => x.queueNum === 0).slice(0, 10)"
+                          :key="i"
+                      >
+                        <v-list-item-content>
+                          <v-list-item-title style="color: yellow">
+                            {{item.track.artists[0]}} - {{item.track.title}} <span class="grey--text float-right">{{item.user}}</span>
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </div>
+                
+                <div>
+                  <span class="overline grey--text">Verzoekjes-pool</span><br>
+                  <v-list dense>
+                    <v-list-item-group class="random-secondary-list">
+                      <v-list-item
+                          v-for="(item, i) in queue.filter(x => x.queueNum === 1)"
+                          :key="i"
+                      >
+                        <v-list-item-content>
+                          <v-list-item-title style="color: orange">
+                            {{item.track.artists[0]}} - {{item.track.title}} <span class="grey--text float-right">{{item.user}}</span>
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                      <v-list-item v-if="queue.filter(x => x.queueNum === 1).length <= 0">
+                        <v-list-item-content>
+                          <v-list-item-title style="color: orange">
+                            De verzoekjes pool is op dit moment leeg... doe snel een verzoekje om hem te vullen!
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </div>
+                <div>
+                  <span class="overline grey--text">Filler watchtrij</span><br>
+                  <v-list dense>
+                    <v-list-item-group>
+                      <v-list-item
+                          v-for="(item, i) in queue.slice(0, 3)"
+                          :key="i"
+                      >
+                        <v-list-item-content>
+                          <v-list-item-title style="color: orangered">
+                            {{item.track.artists[0]}} - {{item.track.title}} <span class="grey--text float-right">{{item.user}}</span>
+                          </v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </div>
+              </div>
+            
+            
+            
+              <div v-else class="normal-queue">
                 <span class="overline grey--text">Wachtrij</span><br>
                 <v-list dense>
-                  <v-list-item-group
-                      class=""
-                  >
+                  <v-list-item-group>
                     <v-list-item
                         v-for="(item, i) in queue.slice(0, 10)"
                         :key="i"
                     >
                       <v-list-item-content>
-                        <v-list-item-title>
+                        <v-list-item-title :style="`color: ${queueTracksColors[item.queueNum]};`">
                           {{i + 1}}. {{item.track.artists[0]}} - {{item.track.title}} <span class="grey--text float-right">{{item.user}}</span>
                         </v-list-item-title>
                       </v-list-item-content>
@@ -37,17 +99,30 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import {Watch} from "vue-property-decorator";
-import {playbackState, userPlaybackInfo} from "@/common/types";
+import {playbackState, trackDto, userPlaybackInfo} from "@/common/types";
+
+interface queueTrack {
+  track: trackDto,
+  user: string,
+  queueNum: number,
+}
 
 @Component({
         name: 'Queue',
     })
-    export default class Queue extends Vue { 
-      private queue = [];
+    export default class Queue extends Vue {
+      private queue: Array<queueTrack> = [];
+      
       private modeColors = [
           "purple",
           "primary",
           "red",
+      ]
+      
+      private queueTracksColors = [
+          "yellow",
+          "orange",
+          "orangered"
       ]
       
       private modeChipColor :string = this.modeColors[0]    
@@ -60,19 +135,24 @@ import {playbackState, userPlaybackInfo} from "@/common/types";
           return this.$store.getters['playbackModule/getPlaybackInfo'];
       }
       
+      
+      get playbackState():playbackState | null{
+        return this.$store.getters['playbackModule/getPlaybackState'];
+      }
+      
       get playbackStateString():string{
-        const state: playbackState | null =  this.$store.getters['playbackModule/getPlaybackState'];
+        const state:playbackState | null = this.playbackState;
         if (state === null) return "Dj-Modues";
         switch (state){
           case 0:
             this.modeChipColor = this.modeColors[0]
-            return "Dj-modus";
+            return "Dj mode";
           case 1:
             this.modeChipColor = this.modeColors[1]
-            return "Verzoekjes-modues"
+            return "Verzoekjes mode"
           case 2:
             this.modeChipColor = this.modeColors[2]
-            return "Random-verzoekjes-modus"
+            return "Random-verzoekjes mode"
         }
       } 
       
@@ -83,21 +163,24 @@ import {playbackState, userPlaybackInfo} from "@/common/types";
               this.playbackInfo.priorityQueuedTracks.forEach((track) => {
                   this.queue.push({
                       track: track,
-                      user: 'DJ'
+                      user: 'DJ',
+                      queueNum: 0,
                   })
               })
-
+            
               this.playbackInfo.secondaryQueuedTracks.forEach((track) => {
                   this.queue.push({
                       track: track,
-                      user: track.user.displayName
+                      user: track.user.displayName,
+                      queueNum: 1,
                   })
               })
 
               this.playbackInfo.fillerQueuedTracks.forEach((track) => {
                   this.queue.push({
                       track: track,
-                      user: 'AutoDJ'
+                      user: 'AutoDJ',
+                      queueNum: 2,
                   })
               })
 
@@ -106,3 +189,9 @@ import {playbackState, userPlaybackInfo} from "@/common/types";
       }
     }
 </script>
+<style scoped>
+  .random-secondary-list {
+    max-height: 400px;
+    overflow: auto;
+  }
+</style>
