17a30f2787d8a9ef347ebe9db22d808f1254627c
pjfm <pjfm@vultr.guest>
2021-01-12 10:13:03 +0100

    changed config and dockerfile

--- ClientApp/vue.config.js
+++ ClientApp/vue.config.js
@@ -6,10 +6,6 @@ module.exports = {
     "vuetify"
   ],
   devServer: {
-    https: {
-      key: fs.readFileSync(path.relative(__dirname, 'server.key')),
-      cert: fs.readFileSync(path.relative(__dirname, 'server.cert')),
-    },
     hotOnly: false,
     port: 8085,
     host: '0.0.0.0',
