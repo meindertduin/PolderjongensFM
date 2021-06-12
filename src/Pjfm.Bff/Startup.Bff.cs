3f965cead4b333f1f04fa61ad2c77c895c7c878b
Meindert van Duin <meindertvanduin99@gmail.com>
2021-06-12 21:21:33 +0200

    serve ClientApp through bff

---
+++ src/Pjfm.Bff/Startup.Bff.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pjfm.Bff
+{
+    public partial class Startup
+    {
+        private void ConfigureBff(IServiceCollection services)
+        {
+            {
+                services.AddControllers();
+                services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
+
+                services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(builder =>
+                    {
+                        builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                    });
+                });
+
+                services.Configure<ForwardedHeadersOptions>(opt =>
+                {
+                    opt.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
+
+                    opt.KnownNetworks.Clear();
+                    opt.KnownProxies.Clear();
+                });
+
+                services.AddHttpContextAccessor();
+                services.AddHttpClient();
+            }
+        }
+
+        private static void RunApiProxy(IApplicationBuilder config, string apiServiceUrl, params string[] scopes)
+        {
+            if (string.IsNullOrEmpty(apiServiceUrl))
+            {
+                throw new ArgumentNullException(nameof(apiServiceUrl));
+            }
+        }
+    }
+}
